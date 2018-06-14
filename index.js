dd771f40253e76c42bc6d399a3c88d6ab8dce4fe
Souji 2613 <puppypoola@gmail.com>
2018-06-14 16:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ dodger.style.left // "180px"
 dodger.style.bottom // "0px"
 dodger.style.bottom = '100px';
 dodger.style.bottom = '0px'
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
